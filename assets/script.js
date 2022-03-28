a107669dc9150f9eace14ec4a9aae6a787290837
Crucix <fchang1720@gmail.com>
2022-03-27 21:12:51 -0500

    added more questions, event listeners

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,15 @@ var questionEl = document.querySelector(".questionTxt");
 var info = document.querySelector(".info");
 var qset = document.querySelector(".display-questions");
 var qset1 = document.querySelector(".display-question1");
+var qset2 = document.querySelector(".display-question2");
 var timerEl = document.querySelector(".timer-count");
-var startBtn = document.querySelector(".start-button");
 var timerTxt = document.querySelector(".timer-text2");
+var startBtn = document.querySelector(".start-button");
+var nextBtn1 = document.querySelector(".next-button1");
+var nextBtn2 = document.querySelector(".next-button2");
+var nextBtn3 = document.querySelector("next-button3");
+var nextBtn4 = document.querySelector("next-button4");
+var buttons = document.querySelector(".buttons");
 
 var chosenQ = "";
 var timer;
@@ -20,10 +26,11 @@ function startGame() {
     qset.setAttribute("style", "display:block");
     qset1.setAttribute("style", "display:block");
     timerCount = 5;
-    
     startBtn.disabled = true;
     startTimer()
-   
+    buttons.addEventListener("click", function(event){
+    nextBtn1.setAttribute("style", "display:block");
+    })
 }
 
 function startTimer() {
@@ -42,4 +49,11 @@ function startTimer() {
 }
 
 startBtn.addEventListener("click", startGame);
+
+nextBtn1.addEventListener("click", function(event) {
+    qset1.setAttribute("style", "display:none");
+    qset2.setAttribute("style", "display:block");
+    nextBtn1.disabled = true;
+});
+
 
