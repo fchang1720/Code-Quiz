3b77198a756ebc9d4f7e511b53b2bd0b80c4ac8e
Crucix <fchang1720@gmail.com>
2022-03-28 19:19:00 -0500

    added form to html, updated functions

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var questionEl = document.querySelector(".questionTxt");
+// Selecting our variables
 var info = document.querySelector(".info");
 var qset = document.querySelector(".display-questions");
 var qset1 = document.querySelector(".display-question1");
@@ -9,21 +9,20 @@ var victory = document.querySelector(".victory");
 var timerEl = document.querySelector(".timer-count");
 var timerTxt = document.querySelector(".timer-text2");
 var startBtn = document.querySelector(".start-button");
-var nextBtn1 = document.querySelector(".next-button1");
-var nextBtn2 = document.querySelector(".next-button2");
-var nextBtn3 = document.querySelector(".next-button3");
-var nextBtn4 = document.querySelector(".next-button4");
 var button1 = document.querySelector(".button1");
 var button2 = document.querySelector(".button2");
 var button3 = document.querySelector(".button3");
 var button4 = document.querySelector(".button4");
-var buttonW = document.querySelectorAll(".buttonW");
 
-var chosenQ = 0;
+// querySelectorAll returns a Nodelist, so we do the following to attach event listener to all buttonW
+let buttonW = document.querySelectorAll(".buttonW");
+buttonW.forEach(buttonW => {
+  buttonW.addEventListener('click', () => timerCount -= 5);
+});
+
+var win = false;
 var timer;
 var timerCount;
-
-
 
 // start function
 function startGame() {
@@ -33,65 +32,62 @@ function startGame() {
     timerCount = 50;
     startBtn.disabled = true;
     startTimer()
-    
+
 }
 
-// function chooseQ() {
-
-// }
-
+function winGame() {
+    localStorage.setItem("timeLeft", timerEl.textcontent)
+}
 
 function startTimer() {
     timer = setInterval(function() {
         timerCount--;
         timerEl.textContent = timerCount;
-        // console.log(timerCount);
-        buttonW.addEventListener("click", function(event){
-            penalize();
-        });
+        if (win && timerCount > 0){
+            clearInterval(timer);
+            timerTxt.textContent = 'is your final score!!'
+            winGame();
+        }
         if (timerCount === 0) {
             clearInterval(timer);
             timerEl.textContent = 'Game Over!'
             timerTxt.textContent = ''
+            loseGame();
         }
-        
+        if (timerCount < 0) {
+            clearInterval(timer);
+            timerEl.textContent = 'Game Over!'
+            timerTxt.textContent = ''
+            loseGame();
+        }
         
     }, 1000);
 }
-function penalize() {
-    timerCount -= 5;
-}  
 
 startBtn.addEventListener("click", startGame);
-
-
 
 button1.addEventListener("click", function(e) {
     qset1.setAttribute("style", "display:none");
     qset2.setAttribute("style", "display:block");
-    nextBtn1.disabled = true;
-    nextBtn1.setAttribute("style", "display:none");
+
 });
 
 button2.addEventListener("click", function(event){
     qset2.setAttribute("style", "display:none");
     qset3.setAttribute("style", "display:block");
-    nextBtn2.disabled = true;
-    nextBtn2.setAttribute("style", "display:none");
+
 });
 
 button3.addEventListener("click", function(event){
     qset3.setAttribute("style", "display:none");
     qset4.setAttribute("style", "display:block");
-    nextBtn3.disabled = true;
-    nextBtn3.setAttribute("style", "display:none");
+
 });
 
 button4.addEventListener("click", function(event){
     qset4.setAttribute("style", "display:none");
     victory.setAttribute("style", "display:block");
-    nextBtn4.disabled = true;
-    nextBtn4.setAttribute("style", "display:none");
-});
+    win = true;
 
+});
 
